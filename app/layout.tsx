2a54dfe4948dfff0c5963208c2f703d64f84e3bc
guestOfficer <3125667110@qq.com>
2025-05-15 16:46:40 +0800

    删除any循环dom添加key

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,16 @@ export default function RootLayout({
   if (!Promise.withResolvers) {
     Promise.withResolvers = function <T>() {
       let resolve: (value: T | PromiseLike<T>) => void = () => {};
-      let reject: (reason: any) => void = () => {};
+      let reject: (reason: unknown) => void = () => {}; // Use 'unknown' instead of 'any'
+      
       const promise = new Promise<T>((res, rej) => {
         resolve = res;
         reject = rej;
       });
+      
       return { promise, resolve, reject };
     };
+    
   }
   return (
     <html lang="en">
--- app/page.tsx
+++ app/page.tsx
@@ -236,46 +236,45 @@ export default function Home() {
                 </button>
               </div>
               <div className="flex flex-wrap justify-center">
-                {Array.from(new Array(numPages), (_, index) => (
-                  <div
-                    onClick={() => {
-                      rotatePDF(index);
-                    }}
-                    className="m-3  flex-[0_0_400px]"
-                    style={{ maxWidth: containerWidth + "px" }}
-                  >
-                    <div className="relative cursor-pointer pdf-page">
-                      <div className="absolute z-10 top-1 right-1 rounded-full p-1 hover:scale-105 hover:fill-white bg-[#ff612f] fill-white">
-                        <svg
-                          className="w-3"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                        >
-                          <path d="M142.9 142.9c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5c0 0 0 0 0 0H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5c7.7-21.8 20.2-42.3 37.8-59.8zM16 312v7.6 .7V440c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l41.6-41.6c87.6 86.5 228.7 86.2 315.8-1c24.4-24.4 42.1-53.1 52.9-83.7c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.2 62.2-162.7 62.5-225.3 1L185 329c6.9-6.9 8.9-17.2 5.2-26.2s-12.5-14.8-22.2-14.8H48.4h-.7H40c-13.3 0-24 10.7-24 24z"></path>
-                        </svg>
-                      </div>
-                      <div className="relative h-full w-full flex flex-col justify-between items-center shadow-md p-3 bg-white hover:bg-gray-50">
-                        <div className="overflow-hidden transition-transform">
-                          <Document
-                            file={pdf}
-                            onLoadSuccess={onDocumentLoadSuccess}
-                          >
-                            {pages.length !== 0 && (
-                              <Page
-                                key={index}
-                                rotate={pages[index].rotate}
-                                width={containerWidth}
-                                renderAnnotationLayer={false}
-                                renderTextLayer={false}
-                                pageNumber={index + 1}
-                              />
-                            )}
-                          </Document>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
+              {Array.from(new Array(numPages), (_, index) => (
+  <div
+    key={index}  // 添加 key 属性
+    onClick={() => {
+      rotatePDF(index);
+    }}
+    className="m-3 flex-[0_0_400px]"
+    style={{ maxWidth: containerWidth + "px" }}
+  >
+    <div className="relative cursor-pointer pdf-page">
+      <div className="absolute z-10 top-1 right-1 rounded-full p-1 hover:scale-105 hover:fill-white bg-[#ff612f] fill-white">
+        <svg
+          className="w-3"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M142.9 142.9c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5c0 0 0 0 0 0H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5c7.7-21.8 20.2-42.3 37.8-59.8zM16 312v7.6 .7V440c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l41.6-41.6c87.6 86.5 228.7 86.2 315.8-1c24.4-24.4 42.1-53.1 52.9-83.7c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.2 62.2-162.7 62.5-225.3 1L185 329c6.9-6.9 8.9-17.2 5.2-26.2s-12.5-14.8-22.2-14.8H48.4h-.7H40c-13.3 0-24 10.7-24 24z"></path>
+        </svg>
+      </div>
+      <div className="relative h-full w-full flex flex-col justify-between items-center shadow-md p-3 bg-white hover:bg-gray-50">
+        <div className="overflow-hidden transition-transform">
+          <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+            {pages.length !== 0 && (
+              <Page
+                key={index} // 保留原来的 key
+                rotate={pages[index].rotate}
+                width={containerWidth}
+                renderAnnotationLayer={false}
+                renderTextLayer={false}
+                pageNumber={index + 1}
+              />
+            )}
+          </Document>
+        </div>
+      </div>
+    </div>
+  </div>
+))}
+
               </div>
             </div>
           )}
